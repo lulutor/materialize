c876d0be387e2cd19278be95c10c3310fedc6892
lulutor <lucas.nandan@gmail.com>
2018-04-18 08:06:46 +0200

    parallax

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-$('.pushpin-demo-nav').each(function() {
+
+
+/*$('.pushpin-demo-nav').each(function() {
 var $this = $(this);
-var $target = $('#' + $(this).attr('data-target'));
+var $target = $('#' + $(this).attr('blue'));
 $this.pushpin({
   top: $target.offset().top,
   bottom: $target.offset().top + $target.outerHeight() - $this.height()
 });
-});
+});*/
 
 
 $(document).ready(function(){
     $('.target').pushpin({
-      top: 0,
-      bottom: 1000,
-      offset: 0
+      top: 80,
+      bottom: 600,
+      offset: 40
     });
 });
+
+$('.tabs-wrapper .row').pushpin('remove');
+
+var elem = document.querySelector('.parallax');
+ var instance = M.Parallax.init(elem, options);
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('.parallax').parallax();
+ });
